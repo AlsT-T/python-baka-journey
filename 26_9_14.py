33a83c05efb3bac2509c743f94a2360d3dcb35cf
baka <1805628949@qq.com>
2026-09-14 21:51:24 +0800

    Day 1: 2.3 字符串

---
+++ 26_9_14.py
@@ -0,0 +1,58 @@
+message = 'Hello Python World!'
+print (message)
+
+message = 'Hello Python Crash Course World!'
+print(message)
+
+name = 'ada lovelace'
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = f'{first_name} {last_name}'
+print(full_name)
+print(f'Hello, {full_name.title()}!')
+
+print("\tPython")
+
+print('Languages:\nPython\nC\nJavaScript')
+
+favorite_language = ' python '
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+favorite_language = favorite_language.strip()
+print(favorite_language) #删除空白
+
+nostarch_url = 'https://nostarch.com'
+print(nostarch_url.rstrip('com'))
+print(nostarch_url.removeprefix('https://'))
+
+name = 'Eric'
+print(f'Hello {name}, would you like to learn some Python today?')
+
+first_name = 'Li'
+last_name = "Hua"
+full_name = f'{first_name} {last_name}'
+print(full_name.title())
+print(full_name.upper())
+print(full_name.lower())
+print(f'{first_name} \n{last_name}')
+print(f'{first_name} \t{last_name}')
+
+print('Vladimir Nabokov once said, "Lolita, light of my life, fire of my loins. My sin, my soul."')
+
+famous_person = 'Zun'
+print(f'{famous_person} once said, "This is good."')
+
+name = ' Takanashi Hoshino '
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+
+filename = 'python_notes.txt'
+print(f'Filename: {filename}')
+print(filename.removesuffix('.txt'))
+
+
